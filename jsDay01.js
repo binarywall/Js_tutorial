3d394546e0b8e0691de4d660790eed22207e6646
binarywall <s98uraj@gmail.com>
2023-10-02 09:09:46 +0530

    JS Day 01

---
+++ jsDay01.js
@@ -0,0 +1,16 @@
+// let a= 1;
+// let b=2;
+// let c=a+b;
+
+let a,b,c,p; // variable declaration.
+
+a=2; //assign a value
+b=3;
+c=a+b;
+
+p=a**b;
+
+console.log('a :',a)
+console.log('b :',b)
+console.log('Sum of a + b = ',c);
+console.log('b power of a = ',p);
