c5125b77e2f5f45edfb8626eeebbd1e75452fecb
binarywall <s98uraj@gmail.com>
2023-10-03 09:14:18 +0530

    update

--- jsDay01.js
+++ jsDay01.js
@@ -13,4 +13,17 @@ p=a**b;
 console.log('a :',a)
 console.log('b :',b)
 console.log('Sum of a + b = ',c);
-console.log('b power of a = ',p);
+// console.log('b power of a = ',p);
+
+
+let a1=19; //assign a value
+let b1=5;
+let sum=a1+b1;
+let multiply=a1*b1;
+let devide=a1/b1;
+
+console.log('a1 :',a1)
+console.log('b1 :',b1)
+console.log('sum of a1 + b1 = ',sum);
+console.log('multiply of a1 x b1 = ',multiply);
+console.log('Devide of a1 / b1 = ',devide);
