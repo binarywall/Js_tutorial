88d27a3e9d268ec6b38c8370fdb82533a7451e90
binarywall <s98uraj@gmail.com>
2023-10-03 09:13:04 +0530

    Basic of Function

---
+++ jsDay02.js
@@ -0,0 +1,32 @@
+// function declaration
+
+// function definition
+
+function functionName (){
+
+    //code or task the function gonna be doing
+
+}
+
+//function call
+functionName();
+
+
+function sum(a,b){
+    let sum=a+b;
+    // console.log("a + b = ",sum)
+    return sum;
+}
+
+let s1=sum(2,3)
+console.log('a+b=',s1)
+console.log('a+b=',sum(5,6))
+
+
+//----------- Task --------- //
+
+// Write  a function to calculate the division of two numbers
+
+// Write a function to calculate the product of 5 numbers ;
+
+// Write a function that takes two numbers,calculate the sum of the numbers in function
