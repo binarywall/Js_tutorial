b25d1d1a995a45860663d03d267703cf8fbd6e11
binarywall <s98uraj@gmail.com>
2023-10-12 09:01:40 +0530

    For Loop

---
+++ jsDay05.js
@@ -0,0 +1,19 @@
+/*
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+*/
+for(let i=0;i<=100;i++){
+    if(i%2!=0){
+        console.log("Odd Number : ",i)
+    }
+}
+
+//write a function to print number 0 to 100 which is divied by 11 
