c8c0d2dd4a139fe50670b54e41d0a667913e46af
binarywall <s98uraj@gmail.com>
2023-11-04 09:32:08 +0530

    string method

--- jsDay10.js
+++ jsDay10.js
@@ -1,4 +1,4 @@
-let str='Hello World! '
+let str='AHello World! '
 let str1='I love cats. Cats are very easy to love. Cats are very popular.'
 let str2='12345'
 let str3='5'
@@ -18,4 +18,14 @@ console.log("String toLowerCase() : ",str1.toLowerCase()); //toLowerCase()
 console.log(str.concat(str1,str2)); // var.concat(var1,var2,...)
 console.log(str3.padStart(3,"0")); // var.padStart(num,"str")
 console.log(str3.padEnd(3,"0")); // var.padEnd(num,"str")
+console.log(str.charAt(2)); // var.chatAt(Index)
+console.log(str.charCodeAt(0)); // var.charCodeAt(Index)
 
+function capitaltoSmall(char){
+    let code=char.charCodeAt(char)
+    // console.log(code);
+    let newCode=code+32
+    let Capital=String.fromCharCode(newCode)
+    console.log(char +"=="+Capital);
+}
+capitaltoSmall('B')
