8370d13a71909000f16ab6415343fbfcddd05fe6
binarywall <suraj.s@devstacksoftsolutions.com>
2024-05-06 20:35:52 +0530

    regular function and arrow function

---
+++ jsDay13.js
@@ -0,0 +1,12 @@
+function sum(a,b){ // Regular  function
+    return a+b;
+}
+console.log(sum(1,2));
+
+const add=(a,b)=>{  //arraow function
+    return a+b;
+}
+console.log(add(1,4));
+
+const add2=(a,b)=>a+b;
+console.log(add2(1,5));
